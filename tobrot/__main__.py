fbefed63662c7e2a5317fb788a2d78882038f89b
Jason-Brody <65779952+Jason-Brody@users.noreply.github.com>
2020-06-11 15:44:54 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -125,13 +125,13 @@ if __name__ == "__main__" :
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
-        filters=Filters.command(["savethumb@ven0m_botl"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["savethumb@ven0m_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(save_thumb_nail_handler)
     #
     clear_thumb_nail_handler = MessageHandler(
         clear_thumb_nail,
-        filters=Filters.command(["clearthumb@ven0m_botl"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["clearthumb@ven0m_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(clear_thumb_nail_handler)
     #
